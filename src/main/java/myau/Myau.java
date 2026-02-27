69054444bc7a6bf91c87171d1e99c207b7956c3b
Cul7ist <bestcultistever@gmail.com>
2026-02-27 18:23:06 -0300

    Fix notifications

--- src/main/java/myau/Myau.java
+++ src/main/java/myau/Myau.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Myau {
-    public static String clientName = "&7[&dSYN&7]&r\n ";
+    public static String clientName = "&7[&dSYN&7]&r ";
     public static String version;
     public static RotationManager rotationManager;
     public static FloatManager floatManager;
