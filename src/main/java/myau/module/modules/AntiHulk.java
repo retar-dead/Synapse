743453070c492d95d426eca69c0bdb57d8a74836
Cul7ist <bestcultistever@gmail.com>
2026-03-02 16:28:11 -0300

    antihulk fixfix

--- src/main/java/myau/module/modules/AntiHulk.java
+++ src/main/java/myau/module/modules/AntiHulk.java
@@ -20,7 +20,6 @@ public class AntiHulk extends Module {
             return;
         }
 
-        // While riding, hold sneak; when not riding, release sneak so player stops sneaking after dismount
         if (mc.thePlayer.isRiding()) {
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindSneak.getKeyCode(), true);
         } else {
